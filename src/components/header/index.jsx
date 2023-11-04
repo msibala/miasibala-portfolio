e46aaba854e2f49cb809deddf8919a6c80c57a42
msibala <50932926+msibala@users.noreply.github.com>
2023-11-03 18:22:33 -0700

    Update index.jsx
    deleting twitter

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -18,7 +18,6 @@ const classes = {
 };
 
 const Header = ({ metadata = {}, noBlog = false }) => {
-//   const twitter = get(metadata, 'author', false);
   const github = get(metadata, 'github', false);
   const linkedin = get(metadata, 'linkedin', false);
 
@@ -38,12 +37,6 @@ const Header = ({ metadata = {}, noBlog = false }) => {
         <ul className={classes.list}>
           {twitter && (
             <li className={classes.item}>
-//               <a
-//                 className={classes.link}
-//                 href={`https://twitter.com/${twitter}`}
-//               >
-//                 Twitter
-//               </a>
             </li>
           )}
           {github && (
