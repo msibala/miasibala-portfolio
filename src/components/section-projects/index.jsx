a99a713b71e5f0a911ba09bb2c2324d0dce0b194
msibala <50932926+msibala@users.noreply.github.com>
2023-12-13 16:36:11 -0800

    Update index.jsx
    commented out projects to see if nav bar will change

--- src/components/section-projects/index.jsx
+++ src/components/section-projects/index.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
+// import React from 'react';
 
-import Section from '../section';
-import SummaryItem from '../summary-item';
+// import Section from '../section';
+// import SummaryItem from '../summary-item';
 
-const SectionProjects = ({ projects }) => {
-  if (!projects.length) return null;
+// const SectionProjects = ({ projects }) => {
+//   if (!projects.length) return null;
 
-  return (
-    <Section title="Projects">
-      {projects.map((project) => (
-        <SummaryItem
-          key={project.name}
-          name={project.name}
-          description={project.description}
-          link={project.link}
-        />
-      ))}
-    </Section>
-  );
-};
+//   return (
+//     <Section title="Projects">
+//       {projects.map((project) => (
+//         <SummaryItem
+//           key={project.name}
+//           name={project.name}
+//           description={project.description}
+//           link={project.link}
+//         />
+//       ))}
+//     </Section>
+//   );
+// };
 
-export default SectionProjects;
+// export default SectionProjects;
